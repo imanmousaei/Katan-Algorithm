1c83a8e0870cc9ae6695b2f9d82b867cd00680a9
ImanMousaei <imanmousaei1379@gmail.com>
2023-03-20 12:31:15 +0330

    Add 2.B

--- HW2/b.py
+++ HW2/b.py
@@ -1,4 +1,5 @@
 # 1734F
+dp = {}
 
 def process_input():
     first_line = list(map(int,input().strip().split()))
@@ -9,31 +10,27 @@ def decimal_to_binary(decimal):
     binary = bin(int(decimal))[2:] # convert to binary and remove "0b" prefix
     return str(binary)
 
-cache = {}
-
 # count of 1's in binary representation of n
-def popcount(n):
+def one_count_in_binary(n):
     binary = decimal_to_binary(n)
     return binary.count('1')
-    # res = 0
-    # while n:
-    #     res += 1
-    #     n &= n - 1
-    # return res
 
 def solve(n, m):
     if m == 0:
         return 0
     if m == 1:
-        return popcount(n) & 1
+        one_count = one_count_in_binary(n)
+        return one_count & 1
     
     if m % 2 == 1:
         t = solve(n, m - 1)
-        x = popcount((m - 1) ^ (n + m - 1)) & 1
-        # print(t, x)
+        last_steps_XOR = (m - 1) ^ (n + m - 1)
+        x = one_count_in_binary(last_steps_XOR) & 1
+        # print(t, x, last_steps_XOR)
         return t + x
-    if (n, m) in cache:
-        return cache[(n, m)]
+    
+    if (n, m) in dp:
+        return dp[(n, m)]
     
     if n % 2 == 0:
         one_cell = 2
@@ -47,14 +44,13 @@ def solve(n, m):
         cnt0 = m - cnt1
     res = one_cell * cnt1 + zero_cell * cnt0
     # print(f'n = {n}, m = {m}, cnt1 = {cnt1}, cnt0 = {cnt0}, one_cell = {one_cell}, zero_cell = {zero_cell}')
-    cache[(n, m)] = res
+    dp[(n, m)] = res
     return res
-
 
 if __name__ == '__main__':
     t = int(input())
     for i in range(t):
-        cache.clear()
+        dp.clear()
         n, m = process_input()
         ans = solve(n, m)
         print(ans)
