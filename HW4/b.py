402ad4fbcde6cc985edce07acb270b93a0e9a6ae
ImanMousaei <imanmousaei1379@gmail.com>
2023-05-05 19:40:54 +0330

    Add constraints

--- HW4/b.py
+++ HW4/b.py
@@ -4,29 +4,41 @@ def process_input():
     return w, b, r
 
 
+verbose = False
+
+
+def printt(*values):
+    if verbose:
+        print(*values)
+
+
 if __name__ == '__main__':
     w, b, r = process_input()
 
     cnt = 0
     colors = [w, b, r]
-    
+
     while (sum(colors) > 0):
         colors = sorted(colors, key=lambda x: -x)
-        
+        printt(colors)
+
         x, y, z = colors
-        dif = min(x-y, y-z, (x-z)/2)
+        dif = min(x/2, y, x-y, y-z, (x-z)/2)
         dif = int(dif)
+
+        if x >= 2 and y >= 1:
+            dif = max(1, dif)
 
         # forming dif shelves of xxy
         cnt += dif
-        # print('dif = ', dif)
         colors[0] -= 2*dif
         colors[1] -= dif
+        # print(f'dif = {dif}, x = {x}')
 
         if dif == 0:
             # forming dif shelves of xyz
             dif = min(colors)
-            # print('dif = 0, new dif = ', dif)
+            printt('dif = 0, new dif = ', dif)
             cnt += dif
             colors[0] -= dif
             colors[1] -= dif
