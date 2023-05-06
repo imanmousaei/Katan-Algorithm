338bf801fe851016d6981b048668a24d2c5d712a
ImanMousaei <imanmousaei1379@gmail.com>
2023-05-06 16:57:57 +0330

    Accept A

--- HW4/a.py
+++ HW4/a.py
@@ -14,44 +14,45 @@ def process_input():
     return n, s, l, r, all
 
 
-# Returns index of x in arr if present, else -1
-def binary_search(arr, low, high, x):
-    if high >= low:
- 
-        mid = (high + low) // 2
- 
-        if arr[mid] == x:
-            return mid
-        elif x < arr[mid]:
-            return binary_search(arr, low, mid - 1, x)
+def check(l_sorted, x):
+    ans = 0
+    cnt = 0
+    for val in l_sorted:
+        if (val[0] > x):
+            cnt += 1
+        elif (val[1] >= x):
+            ans += x - val[0]
+            cnt += 1
+        if (cnt == (n+1)/2):
+            break
+
+    if (cnt < (n+1)/2):
+        return False
+    if (ans > s):
+        return False
+    return True
+
+def binary_search(l_sorted):
+    low = 1
+    high = 1e14
+    while low != high:
+        mid = (high + low + 1) // 2
+
+        if (check(l_sorted, mid)):
+            low = mid
         else:
-            return binary_search(arr, mid + 1, high, x)
- 
-    else:
-        # Element is not present in the array
-        return -1
+            high = mid-1
+    
+    return int(low)
 
 
 if __name__ == '__main__':
     n, s, l, r, all = process_input()
     # get the min l of half elements for the first half. and max r of the 2nd half
-    l_sorted = sorted(all, key=lambda x: x[0])
-    print(l_sorted)
-    
-    if n & 1:
-        middle = (n+1)/2
-    else:
-        middle = n/2
-    
-    middle = int(middle)
-    print(middle)
-        
-    result = [x[0] for x in l_sorted[:middle-1]]
-    s -= sum(result)
-    print(result, s)
-    
-    each = s / (n - middle + 1)
-    print(each)
-    
-        
-    
+    l_sorted = sorted(all, key=lambda x: -x[0])
+    # print(l_sorted)
+
+    s -= sum(l)
+
+    ans = binary_search(l_sorted)
+    print(ans)
